1d14af2eca9332cbe1c05f454abafb4e4b865904
sukritchhabra <sukrit.chhabra@gmail.com>
2015-08-16 21:00:57 -0400

    Created project Template.
    * Might change the way in-section toggling is working. Change that to
    fadeOut/fadeIn(tried it was a little jumpy)

--- script.js
+++ script.js
@@ -2,25 +2,42 @@
 /*jslint node: true*/
 /*global $, jQuery, alert*/
 
-$(function(){
-
+$(document).ready(function() {
     var $window = $(window);
-	var scrollTime = 0.7;
-	var scrollDistance = 150;
+    var scrollTime = 0.4;
+    var scrollDistance = 150;
 
-	$window.on("mousewheel DOMMouseScroll", function(event){
+    $window.on("mousewheel DOMMouseScroll", function(event){
 
-		event.preventDefault();
+        event.preventDefault();
 
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+        var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta*scrollDistance);
 
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,
-				overwrite: 5
-			});
+        TweenMax.to($window, scrollTime, {
+            scrollTo : { y: finalScroll, autoKill:true },
+                ease: Power1.easeOut,
+                overwrite: 5
+            });
 
-	});
+    });
+    
+    $('.moreButton').on('click', function(event) {
+        $(this).siblings('.more').slideToggle('400', function() {
+            $(this).css('display', 'block');
+        });
+        $(this).siblings('.lessButton').slideToggle('400', function() {
+            $(this).css('display', 'block');
+        });
+        $(this).slideToggle('100');
+    });
+
+    $('.lessButton').on('click', function(event) {
+        $(this).siblings('.more').slideToggle('400');
+        $(this).siblings('.moreButton').slideToggle('400', function() {
+            $(this).css('display', 'block');
+        });
+        $(this).slideToggle('100');
+    });
 });
